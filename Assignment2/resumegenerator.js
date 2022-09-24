72950e9bb3b025560b85b0b5a37cef887d1f124e
Taiye Kadiri <taiyefunmilola@gmail.com>
2022-09-24 08:27:09 +0100

    modified the Resume Template

--- Assignment2/resumegenerator.js
+++ Assignment2/resumegenerator.js
@@ -1,6 +1,7 @@
-// // var firstName, lastName, address, phoneNumber, e_mail, about_user, user_skills, user_job_role, job_role_date, company, location, accomplishment, edu_qua, courseOfStudy, studyDate, studyInst, InstLocation, spokenLang, rel_cert;
+var form_FirstName, lastName, address, phoneNumber, e_mail, about_user, user_skills, user_job_role, job_role_date, company, location, accomplishment, edu_qua, courseOfStudy, studyDate, studyInst, InstLocation, spokenLang, rel_cert;
 
-// document.getElementById("cv_firstname").innerHTML = "Taiye";
+// Inputs
+form_FirstName = document.getElementById("formFirstName");
 
 // document.getElementById("cv_lastname").innerHTML = "Kadiri";
 
@@ -9,6 +10,18 @@
 // document.getElementById("cv_num").innerHTML = "+234-56734590";
 
 // document.getElementById("cv_mail").innerHTML = "taiye.kadiri@womentechsters.org";
+
+// Outputs
+var cv_FirstName = document.getElementById('cv_firstname');
+
+
+
+// functions
+function firstNameResult(){
+    cv_FirstName = form_FirstName.value;
+}
+
+form_FirstName.addEventListener("formdata", firstNameResult, false);
 
 
 
---
+++ New_cv_Project/script.js
@@ -0,0 +1,11 @@
+// User names
+function Fullname() {
+    var userFirstName = document.getElementById('firstName');
+    var userLastName = document.getElementById('lastName');
+
+// userFirstName.innerText 
+    var anything = document.getElementById('firstNameInput')
+    
+}
+
+
