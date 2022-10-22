923f7cf156d1ed8fc2fc8b88555e2300063d34f5
Taiye Kadiri <taiyefunmilola@gmail.com>
2022-10-22 08:37:14 +0100

    commit to the showroom project (localstorage)

---
+++ calculator/calculator.js
@@ -0,0 +1,24 @@
+// inputs
+var clear = document.getElementById('clr');
+var openBrac = document.getElementById('obr');
+var percent = document.getElementById('per');
+var divide = document.getElementById('divide');
+var seven = document.getElementById('seven');
+var eight = document.getElementById('eight');
+var nine = document.getElementById('nine');
+var mul = document.getElementById('mul');
+var four = document.getElementById('four');
+var five = document.getElementById('five');
+var six = document.getElementById('six');
+var sub = document.getElementById('sub');
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var add = document.getElementById('add');
+var closeBrac = document.getElementById('cbr');
+var zero = document.getElementById('zero');
+var dec = document.getElementById('dot');
+var equal = document.getElementById('eq');
+
+//Output
+var output = document.getElementById('display')
