8a27abd83f5f8c9213efb740b65dda5a1f625459
Taiye Kadiri <taiyefunmilola@gmail.com>
2022-11-09 07:18:02 +0100

    Revert "creating routers for showroom"
    This reverts commit da8baa141537de823cfc2036a999899a645b8528.

--- Backend_Proper/RecapBackend/Assignment/routes/routers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const controllers = require('../controllers/logic.js')
-const router = express.Router()
-
-router.get('/', controllers.home)
-router.get('/cars/:id',controllers.find)
-router.get('/make/:id',controllers.findMake)
-router.get('/model/:id',controllers.findModel)
-module.exports = {router}
