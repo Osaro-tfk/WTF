2226cc4ee544c3b06b1133ff5d3531ebfbd19cd4
Taiye Kadiri <taiyefunmilola@gmail.com>
2022-11-09 07:57:07 +0100

    creating routers for showroom

---
+++ Backend_Proper/RecapBackend/Assignment/index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const app = express();
+const cors = require('cors');
+const routerManager = require('./routes/routers.js');
+
+app.use(cors()) 
+app.use('/', routerManager.router)
+app.use('/cars', routerManager.router)
+app.use('/make', routerManager.router)
+app.use('/model', routerManager.router)
+app.use('/color', routerManager.router)
+
+app.listen(3004,()=>{
+    console.log('server is running')
+})
---
+++ Backend_Proper/RecapBackend/Assignment/routes/routers.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const controllers = require('../controllers/logic.js')
+const router = express.Router()
+
+router.get('/', controllers.home)
+router.get('/cars/:id', controllers.find)
+router.get('/make/:id', controllers.findMake)
+router.get('/model/:id', controllers.findModel)
+router.get('/color/:id', controllers.findColor)
+module.exports = {router}
