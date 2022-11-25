52fa6d27a5626f736279b8ec9aa3a0754d04ba9b
Taiye Kadiri <taiyefunmilola@gmail.com>
2022-11-25 21:15:23 +0100

    changes to the frontend

--- devWallet/frontend/index.js
+++ devWallet/frontend/index.js
@@ -3,6 +3,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var transactions = [
+    {text:"Lorem Ipsum is simply dummy text of the printing",
+    date:"24 June 2022",
+    due:"Due tomorrow"
+    },
+    {text:"Lorem Ipsum is simply dummy text of the printing",
+    date:"24 June 2022",
+    due:"Due tomorrow"
+    },
+    {text:"Lorem Ipsum is simply dummy text of the printing",
+    date:"24 June 2022",
+    due:"Due tomorrow"
+    },
+    {text:"Lorem Ipsum is simply dummy text of the printing",
+    date:"24 June 2022",
+    due:"Due tomorrow"
+    }
+
+]
+
+var users = [
+    {username:"Eunice", email:"eunicejacob@gmail.com", balance:500},
+    {username:"",email:""}
+]
+
 // this line makes the folders in public accessible...it makes the styles show
 app.use(express.static('public'))
 
@@ -11,16 +36,16 @@ app.set('view engine', 'ejs')
 
 // create routes
 app.get('/',(req,res)=>{
-res.render('index');
+res.render('index',{users: users, trans:transactions});
 })
 
 app.get('/login',(req,res)=>{
     res.render('login');
 })
 
-app.get('/register',(req,res)=>{
-    res.render('index');
-})
+// app.get('/register',(req,res)=>{
+//     res.render('index');
+// })
 
 // run server
 app.listen(port,()=>{
